9f354e4445aa6e9e5d0caf564add3425c0de83a1
Marnissi <Marnissioussama@gmail.com>
2018-03-19 10:21:41 +0100

    config_url

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.db = {};
-config.webhost = 'https://roowix-test.herokuapp.com';
+config.webhost = 'https://roowix-test.herokuapp.com/';
 
 config.db.host = 'roowix:123456@ds115749.mlab.com:15749';
 config.db.name = 'roowix-test';
